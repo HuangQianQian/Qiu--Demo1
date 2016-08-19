6e62ddf9ff950c8e788be70534364e0e23d3003f
QIUGUI <15201314165@163.com>
2016-08-19 11:21:01 +0800

    Initial Commit

---
+++ 白Qiu--Demo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  白Qiu--Demo2
+//
+//  Created by QIUGUI on 16/8/19.
+//  Copyright © 2016年 小兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 白Qiu--Demo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  白Qiu--Demo2
+//
+//  Created by QIUGUI on 16/8/19.
+//  Copyright © 2016年 小兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
